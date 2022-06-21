f3ec9a206fec5d24ee884d018d1ab84bf5176cfe
Qingquan Li <liqqisme@gmail.com>
2022-06-20 23:36:50 -0400

    Add a github corner

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import TableOfContents from './components/TableOfContents';
+import GithubCorner from './components/GithubCorner';
 
 function App() {
   return (
     <div className="container blog-contents-container">
-      <div class="row">
+      <div className="row">
         <TableOfContents />
+        <GithubCorner />
       </div>
     </div>
   );
